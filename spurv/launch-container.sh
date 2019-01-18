b3d11e5c610b241d7772f1e22ac1b1c6179f06a5
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2019-01-18 14:29:21 +0100

    Add more setup to the launcher script

--- spurv/launch-container.sh
+++ spurv/launch-container.sh
@@ -1,7 +1,32 @@
-rm -f /home/tomeu/aosp/vendor
-mkdir -p /home/tomeu/aosp/vendor
-mount -o loop,rw /home/tomeu/vendor.img /home/tomeu/aosp/vendor
-mount -o loop,rw /home/tomeu/userdata.img /home/tomeu/aosp/data
+#!/bin/bash -x
+
+ROOTDIR=`dirname "$0"`
+chmod a+rw /dev/*binder /dev/ashmem
+chmod a+x /sys/kernel/debug/
+chmod a+x /sys/kernel/debug/tracing/
+chmod a+rw /sys/kernel/debug/tracing/trace_marker
+chmod a+rw /sys/kernel/debug/sync/sw_sync
+chmod a+rw /dev/hwrng
+chmod a+rw /dev/dri/*
+chmod a+rw /dev/input/*
+chmod a+rw /sys/power/wake_lock
+chmod a+rw /sys/power/wake_unlock
+chmod a+rw /dev/kmsg
+mkdir -p $ROOTDIR/vendor
+mount -o loop,rw vendor.img $ROOTDIR/vendor
+mount -o loop,rw userdata.img $ROOTDIR/data
+
+[ ! -d /run/user/1000 ] && mkdir -p /run/user/1000
+[ ! -d /var/log/journal ] && mkdir -p /var/log/journal
+[ ! -d /dev/socket ] && mkdir /dev/socket
+XDG_RUNTIME_DIR=/run/user/1000/ weston --tty=7 &
+
+sleep 4 # Wait for /run/user/1000/wayland-0 to be created
+chmod a+rw /run/user/1000/wayland-0
+ls -l /run/user/1000/wayland-0
+
+dd if=$ROOTDIR/selinux-policy.bin of=/sys/fs/selinux/load bs=20M
+setenforce 0
 
 systemd-nspawn --boot \
                --tmpfs /metadata \
@@ -18,10 +43,16 @@ systemd-nspawn --boot \
                --bind /dev/ashmem \
                --bind /dev/urandom \
                --bind /dev/random \
+               --bind /dev/socket \
                --bind /sys/kernel/debug/sync/sw_sync \
                --bind /dev/urandom:/dev/hw_random \
                --bind /sys \
-               --bind /run/user/1000/wayland-0 \
-               --directory `dirname "$0"`
+               --bind /run/user/1000/wayland-0 --register no \
+	       --resolv-conf copy-host \
+	       --link-journal host \
+               --directory $ROOTDIR
 
 # --network-zone=aosp
+
+killall weston
+
