de4bf83cea37e3bb425bc31b96272274dd301652
Robert Foss <robert.foss@collabora.com>
2019-02-26 00:07:53 +0000

    HACK: Enforce device probing order

--- spurv/run.sh
+++ spurv/run.sh
@@ -3,6 +3,15 @@ umount aosp
 sync
 mkdir -p aosp
 mount -o loop,rw aosp.img aosp
+
+# Enforce probing order so that:
+# imx-drm -> /dev/dri/card0
+# etnaviv -> /dev/dri/card1
+echo etnaviv > /sys/bus/platform/drivers/etnaviv/unbind
+echo display-subsystem > /sys/bus/platform/drivers/imx-drm/unbind
+echo display-subsystem > /sys/bus/platform/drivers/imx-drm/bind
+echo etnaviv > /sys/bus/platform/drivers/etnaviv/bind
+
 bash aosp/launch-container.sh
 umount aosp/vendor
 umount aosp/data
